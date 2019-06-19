ae9228b23794a623b091cec1a8349f581f40a4ba
MUGAZ <mugazfb@gmail.com>
2019-06-19 15:51:32 +0400

    Folder fix

--- src/main/java/com/spectrum/appium/com/spectrum/appium/App.java
+++ src/main/java/com/spectrum/appium/com/spectrum/appium/App.java
@@ -81,8 +81,8 @@ public class App{
 //	}
 	
 	@Test
-	public void Device_1() throws Throwable{
-		//Apitest.WebService();
+	public void Device_1(){
+		
 		App k = new App("device1");
 		//APIHandler.API(curtcid, trfold, "Before");
 		//Asnconvertor.Result("971520001714", "820", "USSD_OPT_IN", "Test_Case_ID", "curtcid", "Product_Name", "Test_Scenario_I", "Test_Case", "Confirmation", "Message", "Recharge_Coupon","", "", "", "", "", "", "ExecutionStarttime", "", "");
